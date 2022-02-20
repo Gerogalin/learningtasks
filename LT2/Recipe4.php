290529b2ee4f58425227f80cb26ee85030153900
Jeven Gerogalin <100065163+Gerogalin@users.noreply.github.com>
2022-02-20 16:45:01 +0800

    Add files via upload

---
+++ LT2/Recipe4.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title>Recipe 4</title>
+</head>
+<body>
+<h1>Basic Pasta</h1>
+<img src="Recipe4.jpg" alt="Basic Pasta" title="Basic Pasta">
+
+<h3>Ingredients</h3>
+			<ul>
+				<li>Dried Pasta</li>
+				<li>Water</li>
+				<li>Salt</li>
+				<li>Tomato Puree</li>
+				<li>Cheese/Tuna/Both/None:)</li>
+			</ul>
+
+<h3>Methods</h3>
+			<ol>
+				<li>Boil your pasta adding salt to the water for later on.</li>
+				<li>While pasta is cooking grate your cheese/drain your tuna.</li>
+				<li>When the pasta is cooked add about half a tube of tomato puree, 
+					stirring well, to give you a passata consistency. Of course you can just 
+					use passata if you want, and drain the pasta instead.</li>
+				<li>Then, add the tuna if using any, stirring it in well. Leave for a minute 
+					to heat up the tuna. Add salt/pepper to taste.</li>
+				<li>Serve into a bowl and sprinkle lots of cheese on top.</li>
+			</ol>
+<br>
+<a href="HOME.php">[BACK]</a>
+
+</body>
+</html>
