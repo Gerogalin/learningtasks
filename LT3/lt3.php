e9e751168d4e5ed7cacc84dc77f91c418cec4a57
Jeven Gerogalin <100065163+Gerogalin@users.noreply.github.com>
2022-03-10 21:54:26 +0800

    Add files via upload

---
+++ LT3/lt3.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Top 10 Favorites</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<h1 id="title">My Top 10 Favorite Songs</h1>
+	<div id="col-one">
+		<div id="box-one"><img src="img/enemy.jpg" class="img">
+					<h1>Enemy</h1><h1> - Imagine Dragons</h1></div>
+		<div id="box-two"><img src="img/blackcatcher.jpg" class="img">
+					<h1>Black Catcher</h1><h1> - Vicke Blanka</h1></div>
+		<div id="box-three"><img src="img/sunflower.jpg" class="img">
+					<h1>Sun Flower</h1><h1> - Post Malone</h1></div>
+		<div id="box-four"><img src="img/luciddreams.jpg" class="img">
+					<h1>Lucid Dreams</h1><h1> - Juice Wrld</h1></div>
+		<div id="box-five"><img src="img/allstar.jpg" class="img">
+					<h1>All Star</h1><h1> - Smash Mouth</h1></div>
+	</div>
+	<div id="col-two">
+		<div id="box-six"><img src="img/lifeisahighway.jpg" class="img">
+					<h1>Life is a Highway</h1><h1> - Rascal Flatts</h1></div>
+		<div id="box-seven"><img src="img/pompeii.jpg" class="img">
+					<h1>Pompeii</h1><h1> - Bastille</h1></div>
+		<div id="box-eight"><img src="img/weareyoung.jpg" class="img">
+					<h1>We are Young</h1><h1> - FUN.</h1></div>
+		<div id="box-nine"><img src="img/thenights.jpg" class="img">
+					<h1>The Nights</h1><h1> - Avicii</h1></div>
+		<div id="box-ten"><img src="img/adventureofalifetime.jpg" class="img">
+					<h1>Adventure of a Lifetime</h1><h1> - Coldplay</h1></div>
+	</div>
+</body>
+</html>
