ccac2bd00f12c39548d711c302f6bd882925c53f
Jeven Gerogalin <100065163+Gerogalin@users.noreply.github.com>
2022-03-10 21:02:36 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOME</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id="header">
+		<div><h1 id="name">GEROGALIN<Fname> Jeven</Fname></h1></div>
+		<div id="nav">
+			<ul>
+				<li><a href="index.html">HOME</a></li>
+				<li><a href="">ABOUT</a></li>
+				<li><a href="">CONTACT</a></li>
+			</ul>
+		</div>
+	</div>
+	<div id="content">
+		<div id="col-one">
+			<div id="box-one"><a href="Ltasks.html"><img src="folder.png" class="img"></a><h1 id="title">Learning Tasks</h1></div>
+			<div id="box-two"><img src="folder.png" class="img"><h1 id="title"></h1></div>
+		</div>
+		<div id="col-two">
+			<div id="box-three"><img src="folder.png" class="img"><h1 id="title"></h1></div>
+			<div id="box-four"><img src="folder.png" class="img"><h1 id="title"></h1></div>
+		</div>
+	</div>
+	<div id="footer"></div>
+</body>
+</html>
