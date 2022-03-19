46179bf46e7afd4b50488f808ecaa5d4d0922b00
Jeven Gerogalin <100065163+Gerogalin@users.noreply.github.com>
2022-03-19 21:21:44 +0800

    Add files via upload

---
+++ CLE1MT/gallery.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Culminating Learning Evidence</title>
+	<link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+<body>
+	<div id="wrapper">
+		<div id="header">
+			<div id="title">
+			<div id="logo"><img src="img/logo.png" class="img2"></div>
+			<div id="name"><h1>COVID-19 <un>UNMASKED</un></h1></div>
+			</div>
+			<div id="navigation">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="gallery.php">Gallery</a></li>
+					<li><a href="action.php">Actions or Solutions</a></li>
+					<li><a href="about.php">About Us</a></li>
+				</ul>
+			</div>
+		</div>
+
+		<div id="content">
+			<h1>Covid-19: Field Photos</h1>
+				<div id="p1">
+					<div id="picB"><img src="gallery/p17.jpg" class="picS"></div>
+					<div id="picB"><img src="gallery/p18.jpg" class="picS"></div>
+					<div id="picB"><img src="gallery/p3.jpg" class="picS"></div>
+				 </div>
+				 <div id="i1">
+				 	<div id="picB"><img src="gallery/p22.jpg" class="picS"></div>
+					<div id="picB"><img src="gallery/p23.jpg" class="picS"></div>
+				 </div>
+				 <div id="p2">
+					<div id="picB"><img src="gallery/p19.jpg" class="picS"></div>
+					<div id="picB"><img src="gallery/p20.jpg" class="picS"></div>
+					<div id="picB"><img src="gallery/p12.jpg" class="picS"></div>
+				 </div>
+				 <div id="p3">
+					<div id="picB"><img src="gallery/p16.jpg" class="picS"></div>
+					<div id="picB"><img src="gallery/p8.jpg" class="picS"></div>
+					<div id="picB"><img src="gallery/p6.jpg" class="picS"></div>
+				 </div>
+				 <div id="p4">
+					<div id="picB"><img src="gallery/p10.jpg" class="picS"></div>
+					<div id="picB"><img src="gallery/p11.jpg" class="picS"></div>
+					<div id="picB"><img src="gallery/p9.jpg" class="picS"></div>
+				 </div>
+				 <div id="i2">
+					<div id="picB"><img src="gallery/p24.jpg" class="picS"></div>
+				 </div>
+				 <div id="p5">
+					<div id="picB"><img src="gallery/p13.jpg" class="picS"></div>
+					<div id="picB"><img src="gallery/p14.jpg" class="picS"></div>
+					<div id="picB"><img src="gallery/p15.jpg" class="picS"></div>
+				 </div>
+				 <div id="p6">
+					<div id="picB"><img src="gallery/p1.jpg" class="picS"></div>
+					<div id="picB"><img src="gallery/p7.jpg" class="picS"></div>
+					<div id="picB"><img src="gallery/p2.jpg" class="picS"></div>
+				 </div>
+				<div id="p7">
+					<div id="picB"><img src="gallery/p4.jpg" class="picS"></div>
+					<div id="picB"><img src="gallery/p5.jpg" class="picS"></div>
+					<div id="picB"><img src="gallery/p21.jpg" class="picS"></div>
+				 </div>
+		</div>
+		<div id="footer">
+			<div id="col">
+			<div id="first">
+				<h1>LINKS</h1>
+				<ul>
+					<li>Facebook</li>
+					<li>Youtube</li>
+					<li>Instagram</li>
+					<li>Twitter</li>
+				</ul>
+			</div>
+			<div id="second">
+					<h1> CONTACT US</h1>
+				<ul>
+					<li>Blog</li>
+					<li>About Us</li>
+					<li>News</li>
+					<li>Help</li>
+				</ul>
+			</div>
+			<div id="third"><img src="img/logo.png"></div>
+			<div id="fourth">
+					<h2>COVID-19 UNMASKED</h2>
+				<h1>CLE1: Culminating Learning Evidence</h1>
+				<h1>SOLVING FILIPINO SOCIAL ISSUES THROUGH ONLINE EDUCATION, A WEB SITE CREATION PROJECT</h1>
+				<h1>Created by Jeven Gerogalin 2022</h1>
+			</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
