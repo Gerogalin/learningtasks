3d7dc3792559b2ea90e04d7dc4965d5152658e5d
Jeven Gerogalin <100065163+Gerogalin@users.noreply.github.com>
2022-03-24 14:41:13 +0800

    Update LTasks.php

--- LTasks.php
+++ LTasks.php
@@ -23,6 +23,8 @@
 				<a href="LT3/lt3.php">LT3<a></div>
 			<div id="box">
 				<a href="CLE1MT/index.php">CLE1MT<a></div>
+			<div id="box">
+				<a href="LT4/index.php">LT4<a></div>
 		</div>
 	</div>
 	<div id="footer"></div>
