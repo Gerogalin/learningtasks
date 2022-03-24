af06d5da3d0455c35ffd5f0b900e7f48e864e9bf
Jeven Gerogalin <100065163+Gerogalin@users.noreply.github.com>
2022-03-24 14:43:21 +0800

    Add files via upload

---
+++ LT4/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Red+Hat+Mono:wght@600&display=swap" rel="stylesheet">
+</head>
+<body>
+	<div id="wrapper">
+		<div id="title">
+			<div id="logo"><img src="logo.png" class="img"></div>
+			<div id="pokemon"><h1>P O K E M O N  P O E M</h1></div>
+			<div id="logo"><img src="logo.png" class="img"></div>
+		</div>
+			<div id="body">
+			<div id="tm">
+				<h3>"I WANNA BE THE VERY BEST</h3>
+				<h3>LIKE NO ONE EVER WAS"</h3>
+				<h3>THIS WORDS BRING BACK</h3>
+				<h3>MEMORIES OF TIMES LONG GONE</h3>
+			</div>
+			<div id="tm">
+				<h3>"I TRAVEL ACROSS THE LAND</h3>
+				<h3>SEARCHING FAR AND WIDE"</h3>
+				<h3>WHENEVER I FEEL DOWN AND OUT</h3>
+				<h3>I ENTER THE ESCAPEST PARADISE</h3>
+			</div>
+			<div id="tm">
+				<h3>"I CHOOSE YOU" SAID ASH KETCHUM</h3>
+				<h3>IT FLASHED ON THE TELEVISION SCREEN</h3>
+				<h3>NOW SO MANY YEARS HAVE GONE BY</h3>
+				<h3>BUT THE NOSTALGIA DOESN'T LEAVE</h3>
+			</div>
+			<div id="tm">
+				<h3>WALKING ON MT. CORONET</h3>
+				<h3>AS I TRAVERSE SPACE AND TIME</h3>
+				<h3>"TOO MUCH WATER"</h3>
+				<h3>MAYBE BUT THAT'S WHERE</h3>
+				<h3>THE HOENN SHINES</h3>
+			</div>
+			<div id="tm">
+				<h3>WHENEVER THE WORLD OUTSIDE</h3>
+				<h3>BRINGS THE NEWS OF GLOOM</h3>
+				<h3>I GO TO PALLET TOWN</h3>
+				<h3>AND START A NEW JURNEY FROM MY ROOM</h3>
+			</div>
+			</div>
+	</div>
+</body>
+</html>
